f38a7891037cdab8152986e49fdfd34f9e0ea6e3
Igor Malinovskiy <u.glide@gmail.com>
2015-09-15 16:26:33 -0700

    Update test_connection.cpp

--- tests/unit_tests/test_connection.cpp
+++ tests/unit_tests/test_connection.cpp
@@ -147,7 +147,7 @@ void TestConnection::subscribeAndUnsubscribe()
     connectionPublisher.commandSync("PUBLISH", "ch2", "MSG2");
     connectionPublisher.commandSync("PUBLISH", "ch3", "MSG3");
 
-    wait(3000);
+    wait(5000);
 
     //then
     QCOMPARE(4, messagesRecieved); // Subscribe resp + 3 messages from publisher
