825d2c37ca6ad741235a95d32fb03bbe802383ca
Igor Malinovskiy <u.glide@gmail.com>
2018-10-10 12:05:29 +0300

    Allow to get full raw response

--- src/qredisclient/response.cpp
+++ src/qredisclient/response.cpp
@@ -1,373 +1,322 @@
 #include "response.h"
 #include <QDebug>
-#include <QVector>
 #include <QObject>
 #include <QVariantList>
-#include "qredisclient/utils/text.h"
+#include <QVector>
 #include "qredisclient/utils/compat.h"
+#include "qredisclient/utils/text.h"
 
 const redisReplyObjectFunctions RedisClient::Response::defaultFunctions = {
     RedisClient::Response::createStringObject,
     RedisClient::Response::createArrayObject,
     RedisClient::Response::createIntegerObject,
-    RedisClient::Response::createNilObject,
-    RedisClient::Response::freeObject
-};
+    RedisClient::Response::createNilObject, RedisClient::Response::freeObject};
 
 RedisClient::Response::Response()
-    : m_responseSource(""), 
-      m_redisReader(QSharedPointer<redisReader>(redisReaderCreate(), redisReaderFree)),
-      m_result(nullptr)
-{
-}
-
-RedisClient::Response::Response(const QByteArray & src)
     : m_responseSource(""),
-      m_redisReader(QSharedPointer<redisReader>(redisReaderCreate(), redisReaderFree)),
-      m_result(nullptr)
-{
-    feed(src);
+      m_redisReader(
+          QSharedPointer<redisReader>(redisReaderCreate(), redisReaderFree)),
+      m_result(nullptr) {}
+
+RedisClient::Response::Response(const QByteArray& src)
+    : m_responseSource(""),
+      m_redisReader(
+          QSharedPointer<redisReader>(redisReaderCreate(), redisReaderFree)),
+      m_result(nullptr) {
+  feed(src);
 }
 
-RedisClient::Response::~Response(void)
-{
+RedisClient::Response::~Response(void) {}
+
+void RedisClient::Response::setSource(const QByteArray& src) {
+  reset();
+  feed(src);
 }
 
-void RedisClient::Response::setSource(const QByteArray& src)
-{
-    reset();
-    feed(src);
+void RedisClient::Response::reset() {
+  m_responseSource.clear();
+  m_redisReader =
+      QSharedPointer<redisReader>(redisReaderCreate(), redisReaderFree);
+  m_result.clear();
 }
 
-void RedisClient::Response::reset()
-{
-    m_responseSource.clear();
-    m_redisReader = QSharedPointer<redisReader>(redisReaderCreate(), redisReaderFree);
-    m_result.clear();
+QByteArray RedisClient::Response::source() const { return m_responseSource; }
+
+void RedisClient::Response::appendToSource(const QByteArray& src) { feed(src); }
+
+QByteArray RedisClient::Response::getUnusedBuffer() {
+  if (!hasUnusedBuffer()) return QByteArray{};
+
+  return QByteArray::fromRawData(m_redisReader->buf, m_redisReader->len)
+      .mid(m_redisReader->pos);
 }
 
-QByteArray RedisClient::Response::source() const
-{
+QString RedisClient::Response::toRawString(long limit) const {
+  if (limit <= 0) {
     return m_responseSource;
+  } else {
+    return m_responseSource.left(limit);
+  }
 }
 
-void RedisClient::Response::appendToSource(const QByteArray& src)
-{
-    feed(src);
+bool RedisClient::Response::isEmpty() const {
+  return m_responseSource.isEmpty();
 }
 
-QByteArray RedisClient::Response::getUnusedBuffer()
-{
-    if (!hasUnusedBuffer())
-        return QByteArray{};
-
-    return QByteArray::fromRawData(m_redisReader->buf, m_redisReader->len).mid(m_redisReader->pos);
+RedisClient::Response::Type RedisClient::Response::getType() const {
+  return getResponseType(m_responseSource);
 }
 
-QString RedisClient::Response::toRawString() const
-{
-    return m_responseSource.left(1500);
+QVariant RedisClient::Response::getValue() {
+  if (!m_result.isNull() || parse())  // Return cached result
+    return *m_result;
+
+  return QVariant();
 }
 
-bool RedisClient::Response::isEmpty() const
-{
-    return m_responseSource.isEmpty();
-}
+QVariant* convertUnsafeArray(QVariant* arr) {
+  QVariantList result;
+  auto val = arr->value<QVector<QVariant*>>();
 
-RedisClient::Response::Type RedisClient::Response::getType() const
-{
-    return getResponseType(m_responseSource);
-}
-
-QVariant RedisClient::Response::getValue()
-{
-    if (!m_result.isNull() || parse()) // Return cached result
-        return *m_result;
-       
-    return QVariant();
-}   
-
-QVariant* convertUnsafeArray(QVariant* arr)
-{
-    QVariantList result;
-    auto val = arr->value<QVector<QVariant*>>();
-
-    for (int index=0; index < val.size(); ++index) {
-        if (val[index]->canConvert<QVector<QVariant*>>()) {
-            QScopedPointer<QVariant> subArray(convertUnsafeArray(val[index]));
-            result.append(*subArray);
-        } else {
-            result.append(*val[index]);
-            delete val[index];
-        }
+  for (int index = 0; index < val.size(); ++index) {
+    if (val[index]->canConvert<QVector<QVariant*>>()) {
+      QScopedPointer<QVariant> subArray(convertUnsafeArray(val[index]));
+      result.append(*subArray);
+    } else {
+      result.append(*val[index]);
+      delete val[index];
     }
-    delete arr;
-    return (new QVariant(result));
+  }
+  delete arr;
+  return (new QVariant(result));
 }
 
-bool RedisClient::Response::parse()
-{
-    if (m_responseSource.isEmpty())
-        return false;
+bool RedisClient::Response::parse() {
+  if (m_responseSource.isEmpty()) return false;
 
-    QVariant* reply = nullptr;
+  QVariant* reply = nullptr;
 
-    if (redisReaderGetReply(m_redisReader.data(), (void **)&reply) == REDIS_ERR) {
-        qDebug() << "hiredis cannot parse buffer" << m_redisReader.data()->errstr;
-        qDebug() << "current buffer:" << QByteArray::fromRawData(m_redisReader.data()->buf, m_redisReader.data()->len);
-        qDebug() << "all buffer:" << m_responseSource;
+  if (redisReaderGetReply(m_redisReader.data(), (void**)&reply) == REDIS_ERR) {
+    qDebug() << "hiredis cannot parse buffer" << m_redisReader.data()->errstr;
+    qDebug() << "current buffer:"
+             << QByteArray::fromRawData(m_redisReader.data()->buf,
+                                        m_redisReader.data()->len);
+    qDebug() << "all buffer:" << m_responseSource;
 
-        if (reply)
-            delete reply;
+    if (reply) delete reply;
 
-        return false;
+    return false;
+  }
+
+  if (!reply) return false;
+
+  if (getType() == MultiBulk) {
+    reply = convertUnsafeArray(reply);
+  }
+
+  m_result = QSharedPointer<QVariant>(reply);
+
+  return true;
+}
+
+void RedisClient::Response::feed(const QByteArray& buffer) {
+  m_responseSource.append(buffer);
+
+  if (redisReaderFeed(m_redisReader.data(), buffer.constData(),
+                      buffer.size()) == REDIS_ERR) {
+    qDebug() << "hiredis cannot feed buffer, error:"
+             << m_redisReader.data()->err;
+    qDebug() << "current buffer:"
+             << QByteArray::fromRawData(m_redisReader.data()->buf,
+                                        m_redisReader.data()->len);
+    qDebug() << "buffer part:" << buffer;
+    qDebug() << "all buffer:" << m_responseSource;
+  }
+}
+
+void setParent(const redisReadTask* task, QVariant* r) {
+  QVariant* parent = (QVariant*)task->parent->obj;
+  Q_ASSERT(parent->isValid());
+
+  auto value = parent->value<QVector<QVariant*>>();
+  value[task->idx] = r;
+  parent->setValue(value);
+}
+
+void* RedisClient::Response::createStringObject(const redisReadTask* task,
+                                                char* str, size_t len) {
+  QVariant* s = new QVariant(QByteArray(str, len));
+
+  if (task->parent) setParent(task, s);
+
+  return s;
+}
+
+void* RedisClient::Response::createArrayObject(const redisReadTask* task,
+                                               int elements) {
+  QVariant* arr = new QVariant();
+  arr->setValue(QVector<QVariant*>(elements));
+
+  if (task->parent) setParent(task, arr);
+
+  return arr;
+}
+
+void* RedisClient::Response::createIntegerObject(const redisReadTask* task,
+                                                 long long value) {
+  QVariant* val = new QVariant(value);
+
+  if (task->parent) setParent(task, val);
+
+  return val;
+}
+
+void* RedisClient::Response::createNilObject(const redisReadTask* task) {
+  QVariant* nil = new QVariant();
+
+  if (task->parent) setParent(task, nil);
+
+  return nil;
+}
+
+void RedisClient::Response::freeObject(void* obj) {
+  if (obj == nullptr) return;
+
+  QVariant* o = (QVariant*)obj;
+
+  if (o->canConvert<QVector<QVariant*>>()) {
+    auto val = o->value<QVector<QVariant*>>();
+
+    for (int index = 0; index < val.size(); ++index) {
+      freeObject((void*)val[index]);
     }
+  }
 
-    if (!reply)
-        return false;
+  delete o;
+}
 
-    if (getType() == MultiBulk) {
-        reply = convertUnsafeArray(reply);
+RedisClient::Response::Type RedisClient::Response::getResponseType(
+    const QByteArray& r) const {
+  const char typeChar = (r.length() == 0) ? ' ' : r.at(0);
+  return getResponseType(typeChar);
+}
+
+RedisClient::Response::Type RedisClient::Response::getResponseType(
+    const char typeChar) const {
+  if (typeChar == '+') return Status;
+  if (typeChar == '-') return Error;
+  if (typeChar == ':') return Integer;
+  if (typeChar == '$') return Bulk;
+  if (typeChar == '*') return MultiBulk;
+  return Unknown;
+}
+
+bool RedisClient::Response::isValid() { return m_result || parse(); }
+
+bool RedisClient::Response::isMessage() const {
+  if (!isArray()) return false;
+
+  QVariantList result = m_result->toList();
+
+  return result.size() >= 3 &&
+         (result[0] == "message" || result[0] == "pmessage");
+}
+
+bool RedisClient::Response::isArray() const {
+  return m_result && m_result->canConvert(QMetaType::QVariantList);
+}
+
+bool RedisClient::Response::hasUnusedBuffer() const {
+  return m_result && m_redisReader->pos != m_redisReader->len;
+}
+
+bool RedisClient::Response::isAskRedirect() const {
+  return getResponseType(m_responseSource) == Error &&
+         m_responseSource.startsWith("-ASK");
+}
+
+bool RedisClient::Response::isMovedRedirect() const {
+  return getResponseType(m_responseSource) == Error &&
+         m_responseSource.startsWith("-MOVED");
+}
+
+QByteArray RedisClient::Response::getRedirectionHost() const {
+  if (!isMovedRedirect() && !isAskRedirect()) return QByteArray();
+
+  QByteArray hostAndPort = m_responseSource.split(' ')[2];
+
+  return hostAndPort.split(':')[0];
+}
+
+uint RedisClient::Response::getRedirectionPort() const {
+  if (!isMovedRedirect() && !isAskRedirect()) return 0;
+
+  QByteArray hostAndPort = m_responseSource.split(' ')[2];
+
+  return QString(hostAndPort.split(':')[1]).toUInt();
+}
+
+QByteArray RedisClient::Response::getChannel() const {
+  if (!isMessage()) return QByteArray{};
+
+  QVariantList result = m_result->toList();
+
+  return result[1].toByteArray();
+}
+
+QString RedisClient::Response::valueToHumanReadString(const QVariant& value,
+                                                      int indentLevel) {
+  QString result;
+  QString indent = QString(" ").repeated(indentLevel);
+
+  if (value.isNull()) {
+    result = QString("null");
+  } else if (value.type() == QVariant::Bool) {
+    result = QString(value.toBool() ? "true" : "false");
+  } else if (value.type() == QVariant::StringList) {
+    int index = 1;
+    for (QVariant line : value.toList()) {
+      result.append(
+          QString("%1 %2) %3\r\n")
+              .arg(indent)
+              .arg(QString::number(index++))
+              .arg(QString("\"%1\"").arg(printableString(line.toByteArray()))));
     }
+  } else if (value.type() == QVariant::List ||
+             value.canConvert(QVariant::List)) {
+    QVariantList list = value.value<QVariantList>();
 
-    m_result = QSharedPointer<QVariant>(reply);
-
-    return true;
-}
-
-void RedisClient::Response::feed(const QByteArray& buffer)
-{
-    m_responseSource.append(buffer);
-
-    if (redisReaderFeed(m_redisReader.data(), buffer.constData(), buffer.size()) == REDIS_ERR) {
-        qDebug() << "hiredis cannot feed buffer, error:" << m_redisReader.data()->err;
-        qDebug() << "current buffer:" << QByteArray::fromRawData(m_redisReader.data()->buf, m_redisReader.data()->len);
-        qDebug() << "buffer part:" << buffer;
-        qDebug() << "all buffer:" << m_responseSource;
-    }
-}
-
-void setParent(const redisReadTask * task, QVariant* r)
-{
-    QVariant* parent = (QVariant*)task->parent->obj;
-    Q_ASSERT(parent->isValid());
-
-    auto value = parent->value<QVector<QVariant*>>();
-    value[task->idx] = r;
-    parent->setValue(value);
-}
-
-void *RedisClient::Response::createStringObject(const redisReadTask * task, char *str, size_t len)
-{
-    QVariant* s = new QVariant(QByteArray(str, len));
-
-    if (task->parent)
-        setParent(task, s);
-
-    return s;
-}
-
-void *RedisClient::Response::createArrayObject(const redisReadTask * task, int elements)
-{
-    QVariant* arr = new QVariant();
-    arr->setValue(QVector<QVariant*>(elements));
-
-    if (task->parent)
-       setParent(task, arr);
-
-    return arr;
-}
-
-void *RedisClient::Response::createIntegerObject(const redisReadTask *task, long long value)
-{
-    QVariant* val = new QVariant(value);
-
-    if (task->parent)
-       setParent(task, val);
-
-    return val;
-}
-
-void *RedisClient::Response::createNilObject(const redisReadTask *task)
-{
-    QVariant* nil = new QVariant();
-
-    if (task->parent)
-       setParent(task, nil);
-
-    return nil;
-}
-
-void RedisClient::Response::freeObject(void *obj)
-{
-    if (obj == nullptr)
-        return;
-
-    QVariant* o = (QVariant*)obj;
-
-    if (o->canConvert<QVector<QVariant*>>()) {
-        auto val = o->value<QVector<QVariant*>>();
-
-        for (int index=0; index < val.size(); ++index) {
-            freeObject((void *)val[index]);
-        }
-    }
-
-    delete o;
-}
-
-RedisClient::Response::Type RedisClient::Response::getResponseType(const QByteArray & r) const
-{    
-    const char typeChar = (r.length() == 0)? ' ' : r.at(0);
-    return getResponseType(typeChar);
-}
-
-RedisClient::Response::Type RedisClient::Response::getResponseType(const char typeChar) const
-{    
-    if (typeChar == '+') return Status; 
-    if (typeChar == '-') return Error;
-    if (typeChar == ':') return Integer;
-    if (typeChar == '$') return Bulk;
-    if (typeChar == '*') return MultiBulk;
-    return Unknown;
-}
-
-bool RedisClient::Response::isValid()
-{
-    return m_result || parse();
-}
-
-bool RedisClient::Response::isMessage() const
-{
-    if (!isArray())
-        return false;
-
-    QVariantList result = m_result->toList();
-
-    return result.size() >= 3 && (result[0] == "message" || result[0] == "pmessage");
-}
-
-bool RedisClient::Response::isArray() const
-{
-    return m_result && m_result->canConvert(QMetaType::QVariantList);
-}
-
-bool RedisClient::Response::hasUnusedBuffer() const
-{
-    return m_result && m_redisReader->pos != m_redisReader->len;
-}
-
-bool RedisClient::Response::isAskRedirect() const
-{
-    return getResponseType(m_responseSource) == Error
-            && m_responseSource.startsWith("-ASK");
-}
-
-bool RedisClient::Response::isMovedRedirect() const
-{
-    return getResponseType(m_responseSource) == Error
-            && m_responseSource.startsWith("-MOVED");
-}
-
-QByteArray RedisClient::Response::getRedirectionHost() const
-{
-    if (!isMovedRedirect() && !isAskRedirect())
-        return QByteArray();
-
-    QByteArray hostAndPort = m_responseSource.split(' ')[2];
-
-    return hostAndPort.split(':')[0];
-}
-
-uint RedisClient::Response::getRedirectionPort() const
-{
-    if (!isMovedRedirect() && !isAskRedirect())
-        return 0;
-
-    QByteArray hostAndPort = m_responseSource.split(' ')[2];
-
-    return QString(hostAndPort.split(':')[1]).toUInt();
-}
-
-QByteArray RedisClient::Response::getChannel() const
-{
-    if (!isMessage())
-        return QByteArray{};
-
-    QVariantList result = m_result->toList();
-
-    return result[1].toByteArray();
-}
-
-QString RedisClient::Response::valueToHumanReadString(const QVariant& value, int indentLevel)
-{
-    QString result;
-    QString indent = QString(" ").repeated(indentLevel);
-
-    if (value.isNull())
-    {
-        result = QString("null");
-    }
-    else if (value.type() == QVariant::Bool)
-    {
-        result = QString(value.toBool() ? "true" : "false");
-    }
-    else if (value.type() == QVariant::StringList)
-    {
-        int index = 1;
-        for (QVariant line : value.toList()) {
-            result.append(
-                QString("%1 %2) %3\r\n")
+    int index = 1;
+    for (QVariant item : list) {
+      result.append(QString("%1 %2) %3\r\n")
                         .arg(indent)
                         .arg(QString::number(index++))
-                        .arg(QString("\"%1\"").arg(printableString(line.toByteArray())))
-                        );
-        }
+                        .arg(valueToHumanReadString(item, indentLevel + 1)));
     }
-    else if (value.type() == QVariant::List || value.canConvert(QVariant::List))
-    {
-        QVariantList list = value.value<QVariantList>();
+  } else {
+    result = QString("\"%1\"").arg(printableString(value.toByteArray()));
+  }
 
-        int index = 1;
-        for (QVariant item : list)
-        {
-            result.append(
-                QString("%1 %2) %3\r\n")
-                        .arg(indent)
-                        .arg(QString::number(index++))
-                        .arg(valueToHumanReadString(item, indentLevel + 1))
-                        );
-        }
-    }
-    else
-    {
-        result = QString("\"%1\"").arg(printableString(value.toByteArray()));
-    }
-
-    return indent + result;
+  return indent + result;
 }
 
-bool RedisClient::Response::isErrorMessage() const
-{
-    return getResponseType(m_responseSource) == Error
-            && m_responseSource.startsWith("-ERR");
+bool RedisClient::Response::isErrorMessage() const {
+  return getResponseType(m_responseSource) == Error &&
+         m_responseSource.startsWith("-ERR");
 }
 
-bool RedisClient::Response::isErrorStateMessage() const
-{
-    return getResponseType(m_responseSource) == Error
-                && (m_responseSource.startsWith("-DENIED")
-                    || m_responseSource.startsWith("-LOADING")
-                    || m_responseSource.startsWith("-MISCONF"));
+bool RedisClient::Response::isErrorStateMessage() const {
+  return getResponseType(m_responseSource) == Error &&
+         (m_responseSource.startsWith("-DENIED") ||
+          m_responseSource.startsWith("-LOADING") ||
+          m_responseSource.startsWith("-MISCONF"));
 }
 
-bool RedisClient::Response::isDisabledCommandErrorMessage() const
-{
-    return isErrorMessage() && m_responseSource.contains("unknown command");
+bool RedisClient::Response::isDisabledCommandErrorMessage() const {
+  return isErrorMessage() && m_responseSource.contains("unknown command");
 }
 
-bool RedisClient::Response::isOkMessage() const
-{
-    return getResponseType(m_responseSource) == Status
-            && m_responseSource.startsWith("+OK");
+bool RedisClient::Response::isOkMessage() const {
+  return getResponseType(m_responseSource) == Status &&
+         m_responseSource.startsWith("+OK");
 }
--- src/qredisclient/response.h
+++ src/qredisclient/response.h
@@ -1,87 +1,87 @@
 #pragma once
+#include <hiredis/read.h>
 #include <QByteArray>
+#include <QSharedPointer>
+#include <QString>
 #include <QVariant>
 #include <QVector>
-#include <QString>
-#include <QSharedPointer>
-#include <hiredis/read.h>
 
 #include "exception.h"
 
 namespace RedisClient {
-class Response
-{    
-    ADD_EXCEPTION
+class Response {
+  ADD_EXCEPTION
 
-public:
-    enum Type { Status, Error, Integer, Bulk, MultiBulk, Unknown };
+ public:
+  enum Type { Status, Error, Integer, Bulk, MultiBulk, Unknown };
 
-public:
-    Response();
-    Response(const QByteArray &);
-    virtual ~Response(void);
+ public:
+  Response();
+  Response(const QByteArray &);
+  virtual ~Response(void);
 
-    QVariant getValue();        
-    Type getType() const;
-    QByteArray source() const;
-    QString toRawString() const;
+  QVariant getValue();
+  Type getType() const;
+  QByteArray source() const;
+  QString toRawString(long limit = 1500) const;
 
-    bool isEmpty() const;
-    bool isErrorMessage() const;
-    bool isErrorStateMessage() const;
-    bool isDisabledCommandErrorMessage() const;
-    bool isOkMessage() const;
-    bool isValid();
-    bool isMessage() const;
-    bool isArray() const;
-    bool hasUnusedBuffer() const;
+  bool isEmpty() const;
+  bool isErrorMessage() const;
+  bool isErrorStateMessage() const;
+  bool isDisabledCommandErrorMessage() const;
+  bool isOkMessage() const;
+  bool isValid();
+  bool isMessage() const;
+  bool isArray() const;
+  bool hasUnusedBuffer() const;
 
-    // Pub/Sub support
-    QByteArray getChannel() const;
+  // Pub/Sub support
+  QByteArray getChannel() const;
 
-    // Cluster support
-    bool isAskRedirect() const;
-    bool isMovedRedirect() const;
-    QByteArray getRedirectionHost() const;
-    uint getRedirectionPort() const;
+  // Cluster support
+  bool isAskRedirect() const;
+  bool isMovedRedirect() const;
+  QByteArray getRedirectionHost() const;
+  uint getRedirectionPort() const;
 
-public:
-    void setSource(const QByteArray&);    
-    void appendToSource(const QByteArray&);
-    QByteArray getUnusedBuffer();
-    void reset();
+ public:
+  void setSource(const QByteArray &);
+  void appendToSource(const QByteArray &);
+  QByteArray getUnusedBuffer();
+  void reset();
 
-    static QString valueToHumanReadString(const QVariant&, int indentLevel=0);
+  static QString valueToHumanReadString(const QVariant &, int indentLevel = 0);
 
-protected:
-    Type getResponseType(const QByteArray&) const;
-    Type getResponseType(const char) const;
+ protected:
+  Type getResponseType(const QByteArray &) const;
+  Type getResponseType(const char) const;
 
-    bool parse();
-    void feed(const QByteArray &buffer);
+  bool parse();
+  void feed(const QByteArray &buffer);
 
-protected:
-    QByteArray m_responseSource;
-    QSharedPointer<redisReader> m_redisReader;
-    QSharedPointer<QVariant> m_result;
+ protected:
+  QByteArray m_responseSource;
+  QSharedPointer<redisReader> m_redisReader;
+  QSharedPointer<QVariant> m_result;
 
-private:
-    /*
-     * hiredis custom functions
-     */
-    static void *createStringObject(const redisReadTask *task, char *str, size_t len);
-    static void *createArrayObject(const redisReadTask *t, int elements);
-    static void *createIntegerObject(const redisReadTask *task, long long value);
-    static void *createNilObject(const redisReadTask *task);
-    static void freeObject(void *obj);
+ private:
+  /*
+   * hiredis custom functions
+   */
+  static void *createStringObject(const redisReadTask *task, char *str,
+                                  size_t len);
+  static void *createArrayObject(const redisReadTask *t, int elements);
+  static void *createIntegerObject(const redisReadTask *task, long long value);
+  static void *createNilObject(const redisReadTask *task);
+  static void freeObject(void *obj);
 
-    static const redisReplyObjectFunctions defaultFunctions;
+  static const redisReplyObjectFunctions defaultFunctions;
 
-    static redisReader *redisReaderCreate(void)
-    {
-        return redisReaderCreateWithFunctions(const_cast<redisReplyObjectFunctions*>(&defaultFunctions));
-    }
+  static redisReader *redisReaderCreate(void) {
+    return redisReaderCreateWithFunctions(
+        const_cast<redisReplyObjectFunctions *>(&defaultFunctions));
+  }
 };
-}
+}  // namespace RedisClient
 
-Q_DECLARE_METATYPE(QVector<QVariant*>)
+Q_DECLARE_METATYPE(QVector<QVariant *>)
