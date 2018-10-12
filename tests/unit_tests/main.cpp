f5bc488fdeaa37d88249c29a74be11223f33585a
Igor Malinovskiy <u.glide@gmail.com>
2018-10-12 10:34:19 +0300

    Clean up unit tests

--- tests/unit_tests/main.cpp
+++ tests/unit_tests/main.cpp
@@ -1,43 +1,36 @@
-#include <QTest>
 #include <QCoreApplication>
+#include <QTest>
 
-//tests
+// tests
 #include <iostream>
-#include "test_command.h"
-#include "test_response.h"
-#include "test_connection.h"
-#include "test_ssh.h"
-#include "test_config.h"
-#include "test_text.h"
 #include "qredisclient/redisclient.h"
+#include "test_command.h"
+#include "test_config.h"
+#include "test_connection.h"
+#include "test_response.h"
+#include "test_text.h"
 
-int main(int argc, char *argv[])
-{
-    QCoreApplication app( argc, argv );
+int main(int argc, char *argv[]) {
+  QCoreApplication app(argc, argv);
 
-    initRedisClient();
+  initRedisClient();
 
-    QScopedPointer<QObject> testCommand(new TestCommand);
-    QScopedPointer<QObject> testSsh(new TestSsh);
-    QScopedPointer<QObject> testResponse(new TestResponse);
-    QScopedPointer<QObject> testConfig(new TestConfig);
-    QScopedPointer<QObject> testText(new TestText);
-    QScopedPointer<QObject> testConnection(new TestConnection);
+  QScopedPointer<QObject> testCommand(new TestCommand);
+  QScopedPointer<QObject> testResponse(new TestResponse);
+  QScopedPointer<QObject> testConfig(new TestConfig);
+  QScopedPointer<QObject> testText(new TestText);
+  QScopedPointer<QObject> testConnection(new TestConnection);
 
-    int allTestsResult = 0
-            + QTest::qExec(testCommand.data(), argc, argv)
-            + QTest::qExec(testSsh.data(), argc, argv)
-            + QTest::qExec(testResponse.data(), argc, argv)
-            + QTest::qExec(testConfig.data(), argc, argv)
-            + QTest::qExec(testText.data(), argc, argv)
-            + QTest::qExec(testConnection.data(), argc, argv)
-            ;
+  int allTestsResult = 0 + QTest::qExec(testCommand.data(), argc, argv) +
+                       QTest::qExec(testResponse.data(), argc, argv) +
+                       QTest::qExec(testConfig.data(), argc, argv) +
+                       QTest::qExec(testText.data(), argc, argv) +
+                       QTest::qExec(testConnection.data(), argc, argv);
 
-    if (allTestsResult == 0)
-        qDebug() << "[Tests PASS]";
-    else
-        qDebug() << "[Tests FAIL]";
+  if (allTestsResult == 0)
+    qDebug() << "[Tests PASS]";
+  else
+    qDebug() << "[Tests FAIL]";
 
-    return (allTestsResult != 0 )? 1 : 0;
+  return (allTestsResult != 0) ? 1 : 0;
 }
-
